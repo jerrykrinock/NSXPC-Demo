92887f91f2f4491b250bd52dfdd2ad9cb28c1e7e
Jerry Krinock <jerry@sheepsystems.com>
2019-03-05 00:29:46 -0800

    Removed unnecessary files, groups, and menus.

--- MainApp/Job.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface Job : NSObject <NSSecureCoding> {
-}
-
-@property (copy) NSString* answer;
-
-@end
--- MainApp/Worker.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "Job.h"
-
-@protocol Worker
-
-- (void)doWorkOn:(NSString*)textIn
-          thenDo:(void (^)(Job *job))job;
-
-@end
